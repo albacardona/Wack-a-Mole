908f9c0c50dfa75bf287ba4edddad9bd536646d4
Alba Cardona <alba.cardonajimenez@gmail.com>
2025-04-05 11:45:17 +0200

    feat: add button component with variants and animations

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,51 @@
-export const Button = () => (
-  <div>
-    
-  </div>
+import { cva, type VariantProps } from 'class-variance-authority';
+import clsx from 'clsx';
+
+const buttonVariants = cva(
+  'cursor-none py-3 px-2 text-sm font-game rounded-md transition-all duration-150 ease-in-out active:translate-y-2 active:border-b-[0px] border-b-[1px]',
+  {
+    variants: {
+      variant: {
+        default:
+          'bg-bg-secondary border-bg-secondary text-fg-primary shadow-button-primary active:shadow-button-primary-active',
+        accent:
+          'bg-accent border-accent text-fg-primary shadow-button-accent active:shadow-button-accent-active',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  },
 );
+
+interface ButtonProps {
+  variant?: VariantProps<typeof buttonVariants>['variant'];
+  children?: React.ReactNode;
+  value: number | undefined;
+  isActive?: boolean;
+  className?: string;
+  onClick: (value: number) => void;
+}
+
+export const Button = ({ value, children, className, variant, isActive, onClick }: ButtonProps) => {
+  const handleClick = () => {
+    if (value !== undefined) {
+      (onClick as (value: number) => void)(value);
+    } else {
+      (onClick as () => void)();
+    }
+  };
+
+  return (
+    <button
+      type="button"
+      className={clsx(
+        { 'w-auto': true, 'bg-bg-secondary-light shadow-none translate-y-2': isActive },
+        buttonVariants({ variant, className }),
+      )}
+      onClick={handleClick}
+    >
+      {children ?? value}
+    </button>
+  );
+};
