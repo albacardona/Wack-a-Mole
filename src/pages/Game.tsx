62b36ae0007500959d4a1696bf42a7bdfd7c1f1a
Alba Cardona <alba.cardonajimenez@gmail.com>
2025-04-06 13:30:46 +0200

    chore: remove unused function

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -6,7 +6,7 @@ import { useTranslation } from 'react-i18next';
 
 export const Game = () => {
   const { t } = useTranslation();
-  const { moles, showMoleRandomly, handleClickStartGame, hideMole } = useGame();
+  const { moles, handleClickStartGame, hideMole } = useGame();
 
   const handleClickMole = (id: string) => {
     hideMole(id);
